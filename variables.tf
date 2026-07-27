1170f41b4dbfd7e6dd5cd517929cb2365e81b47c
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2026-07-27 17:11:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,14 +13,14 @@ variable "aws_secret_key" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 
 variable "ami" {
    type        = string
-   description = "Ubuntu AMI ID"
-   default     = "ami-0b6c6ebed2801a5cb"
+   description = " AMI ID"
+   default     = "ami-0aa939abad20cdeae"
 }
 
 variable "instance_type" {
